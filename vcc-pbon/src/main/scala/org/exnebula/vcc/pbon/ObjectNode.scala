ee5000424238a599ba8da9154b42082a0a712d63
Thomas Santana <mailleux@gmail.com>
2011-08-16 13:04:24 -0300

    Renamed classes in ObjectNode to reflect original intent.

--- vcc-pbon/src/main/scala/org/exnebula/vcc/pbon/ObjectNode.scala
+++ vcc-pbon/src/main/scala/org/exnebula/vcc/pbon/ObjectNode.scala
@@ -19,7 +19,7 @@ package org.exnebula.vcc.pbon
 import org.exnebula.protobuf.vcc.ObjectNotation
 import com.google.protobuf.GeneratedMessage.GeneratedExtension
 
-trait Field {
+trait Datum {
   val name: String
 
   protected def serializePayload(db: ObjectNotation.Datum.Builder)
@@ -32,17 +32,17 @@ trait Field {
   }
 }
 
-object Field {
+object Datum {
 
-  class Extractor[T](extension: GeneratedExtension[ObjectNotation.Datum, T], builder: (String, T) => Field) {
-    def unapply(datum: ObjectNotation.Datum): Option[Field] = {
+  class Extractor[T](extension: GeneratedExtension[ObjectNotation.Datum, T], builder: (String, T) => Datum) {
+    def unapply(datum: ObjectNotation.Datum): Option[Datum] = {
       if (datum.hasExtension(extension)) Some(builder(datum.getName, datum.getExtension(extension)))
       else None
     }
   }
 
-  class ListExtractor[T](extension: GeneratedExtension[ObjectNotation.Datum, java.util.List[T]], builder: (String, List[T]) => Field) {
-    def unapply(datum: ObjectNotation.Datum): Option[Field] = {
+  class ListExtractor[T](extension: GeneratedExtension[ObjectNotation.Datum, java.util.List[T]], builder: (String, List[T]) => Datum) {
+    def unapply(datum: ObjectNotation.Datum): Option[Datum] = {
       if (datum.getExtensionCount(extension) > 0) {
         val l = for (i <- 0 until datum.getExtensionCount(extension)) yield {
           datum.getExtension(extension, i)
@@ -53,40 +53,58 @@ object Field {
     }
   }
 
-  object ExtractIntField extends Extractor[ObjectNotation.IntDatum](ObjectNotation.intDatum, (name, datum) => IntField(name, datum.getValue))
+  object AsIntDatum extends Extractor[ObjectNotation.IntDatum](ObjectNotation.intDatum, (name, datum) => IntDatum(name, datum.getValue))
 
-  object ExtractStringField extends Extractor[ObjectNotation.StringDatum](ObjectNotation.stringDatum, (name, datum) => StringField(name, datum.getValue))
+  object AsStringDatum extends Extractor[ObjectNotation.StringDatum](ObjectNotation.stringDatum, (name, datum) => StringDatum(name, datum.getValue))
 
-  object ExtractObjectField extends Extractor[ObjectNotation.ObjectDatum](ObjectNotation.objectDatum, (name, datum) => ObjectField(name, NodeObject.deserialize(datum.getValue)))
+  object AsObjectDatum extends Extractor[ObjectNotation.ObjectDatum](ObjectNotation.objectDatum, (name, datum) => ObjectDatum(name, ObjectNode.deserialize(datum.getValue)))
 
-  object ExtractObjectListField extends ListExtractor[ObjectNotation.ObjectDatum](ObjectNotation.objectData, (name, data) => {
-    ObjectListField(name, data.map(node => NodeObject.deserialize(node.getValue)))
+  object AsObjectData extends ListExtractor[ObjectNotation.ObjectDatum](ObjectNotation.objectData, (name, data) => {
+    ObjectData(name, data.map(node => ObjectNode.deserialize(node.getValue)))
   })
 
-  def deserialize(datum: ObjectNotation.Datum): Field = {
+  def deserialize(datum: ObjectNotation.Datum): Datum = {
     datum match {
-      case ExtractIntField(field) => field
-      case ExtractStringField(field) => field
-      case ExtractObjectField(field) => field
-      case ExtractObjectListField(field) => field
+      case AsIntDatum(field) => field
+      case AsStringDatum(field) => field
+      case AsObjectDatum(field) => field
+      case AsObjectData(field) => field
       case _ => null
     }
   }
 }
 
-case class IntField(name: String, value: Int) extends Field {
+case class IntDatum(name: String, value: Int) extends Datum {
   protected def serializePayload(db: ObjectNotation.Datum.Builder) {
     db.setExtension(ObjectNotation.intDatum, ObjectNotation.IntDatum.newBuilder().setValue(value).build())
   }
 }
 
-case class StringField(name: String, value: String) extends Field {
+case class StringDatum(name: String, value: String) extends Datum {
   protected def serializePayload(db: ObjectNotation.Datum.Builder) {
     db.setExtension(ObjectNotation.stringDatum, ObjectNotation.StringDatum.newBuilder().setValue(value).build())
   }
 }
 
-case class NodeObject(buildName: String, version: Int, fields: List[Field]) {
+case class ObjectDatum(name: String, value: ObjectNode) extends Datum {
+  protected def serializePayload(db: ObjectNotation.Datum.Builder) {
+    db.setExtension(ObjectNotation.objectDatum, ObjectNotation.ObjectDatum.newBuilder().setValue(value.serializer).build())
+  }
+}
+
+case class ObjectData(name: String, value: List[ObjectNode]) extends Datum {
+  protected def serializePayload(db: ObjectNotation.Datum.Builder) {
+    for (node <- value) {
+      db.addExtension(ObjectNotation.objectData, ObjectNotation.ObjectDatum.newBuilder().setValue(node.serializer).build())
+    }
+  }
+}
+
+object ObjectData {
+  def apply(name: String, ns: ObjectNode*): ObjectData = new ObjectData(name, ns.toList)
+}
+
+case class ObjectNode(buildName: String, version: Int, fields: List[Datum]) {
   def serializer: ObjectNotation.ObjectNode.Builder = {
     val ob = ObjectNotation.ObjectNode.newBuilder()
     ob.setBuildClass(buildName)
@@ -98,31 +116,14 @@ case class NodeObject(buildName: String, version: Int, fields: List[Field]) {
   }
 }
 
-object NodeObject {
-  def deserialize(oNode: ObjectNotation.ObjectNode): NodeObject = {
+object ObjectNode {
+  def deserialize(oNode: ObjectNotation.ObjectNode): ObjectNode = {
     val builder = oNode.getBuildClass
     val version = oNode.getVersion
-    val fields: Seq[Field] = for (x <- (0 until oNode.getDataCount)) yield {
-      Field.deserialize(oNode.getData(x))
+    val fields: Seq[Datum] = for (x <- (0 until oNode.getDataCount)) yield {
+      Datum.deserialize(oNode.getData(x))
     }
-    NodeObject(builder, version, fields.toList)
+    ObjectNode(builder, version, fields.toList)
   }
 }
 
-case class ObjectField(name: String, value: NodeObject) extends Field {
-  protected def serializePayload(db: ObjectNotation.Datum.Builder) {
-    db.setExtension(ObjectNotation.objectDatum, ObjectNotation.ObjectDatum.newBuilder().setValue(value.serializer).build())
-  }
-}
-
-case class ObjectListField(name: String, value: List[NodeObject]) extends Field {
-  protected def serializePayload(db: ObjectNotation.Datum.Builder) {
-    for (node <- value) {
-      db.addExtension(ObjectNotation.objectData, ObjectNotation.ObjectDatum.newBuilder().setValue(node.serializer).build())
-    }
-  }
-}
-
-object ObjectListField {
-  def apply(name: String, ns: NodeObject*): ObjectListField = new ObjectListField(name, ns.toList)
-}
